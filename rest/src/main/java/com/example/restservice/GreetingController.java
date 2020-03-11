2773aad2235d8e0e086e24a5e5c026bb95c5d1d9
Daniel Rosowski <daniel.rosowski.extern@postadress.de>
2020-03-11 13:24:21 +0100

    Apply CORS settings

--- rest/src/main/java/com/example/restservice/GreetingController.java
+++ rest/src/main/java/com/example/restservice/GreetingController.java
@@ -10,6 +10,7 @@ public class GreetingController {
 	private String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
